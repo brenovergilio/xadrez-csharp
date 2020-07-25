4b2db9f590a434feb656d07ce2f10e8e3d766bf5
Breno Vergilio <bjvergilio47@gmail.com>
2020-07-25 16:23:25 -0400

    Classe Tabuleiro adicionada

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+namespace tabuleiro
+{
+    public class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QntMovimentos {get; protected set;}
+        public Tabuleiro Tab {get; protected set;}
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Tab = tab;
+            Cor = cor;
+            QntMovimentos = 0;
+        }
+    }
+}
