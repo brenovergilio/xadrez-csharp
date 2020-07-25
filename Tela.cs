034fcb530544e69b8e4cd0b7f5604783ddb3ce6e
Breno Vergilio <bjvergilio47@gmail.com>
2020-07-25 17:29:04 -0400

    Melhorando a implementacao do metodo imprimir tabuleiro

--- Tela.cs
+++ Tela.cs
@@ -9,15 +9,33 @@ namespace C__and_.Net
         {
             for(int i=0;i<tab.Linhas;i++)
             {
+                System.Console.Write(8-i+" ");
                 for(int j=0;j<tab.Colunas;j++)
                 {
                     if(tab.PecaEspecifica(i,j) == null)
                         System.Console.Write("- ");
                     else
-                        System.Console.Write(tab.PecaEspecifica(i,j) + " ");
+                    {
+                        ImprimirPeca(PecaEspecifica(i,j));
+                        System.Console.Write(" ");
+                    }
                 }
                 System.Console.WriteLine();
             }
+            System.Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca p)
+        {
+            if(p.Cor == Cor.Branca)
+                System.Console.Write(p);
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(p);
+                Console.ForegroundColor = aux;
+            }    
         }
     }
 }
