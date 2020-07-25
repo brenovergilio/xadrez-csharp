6fdd5e59c29e903c28e4fa58882850b8285cb2f5
Breno Vergilio <bjvergilio47@gmail.com>
2020-07-25 16:00:29 -0400

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace C__and_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
